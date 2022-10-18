9ccebb80f15e227b0be945adbe4b3fe91ab4c3b4
marstMUNI <483894@mail.muni.cz>
2022-10-18 12:49:00 +0000

    working demo units

--- src/ultimate_pyfoam/domain/of_units.py
+++ src/ultimate_pyfoam/domain/of_units.py
@@ -9,13 +9,12 @@ class OF_units():
     def unit(self):
         units = ['kg','m','s','K','mol','A','cd']
         for u in units:
-            if u in self.SIunit:
-                z = re.search("("+f"[{u}]"+"{"+f"{len(u)}"+"})([0-9\\-]{1,5})", self.SIunit)
-                #print(z)
-                self.values.append(z[2])
-            else: self.values.append(0)
+            z = re.search("(" + f"[{u}]" + "{" + f"{len(u)}" + "})([0-9\\-]{1,5})", self.SIunit)
+            if z is None:
+                self.values.append('0')
+            else: self.values.append(z[2])
         return self.values
 
     def __str__(self):
-        string = f'[ {self.unit()[0]} {self.unit()[1]} {self.unit()[2]} {self.unit()[3]} {self.unit()[4]} {self.unit()[5]} {self.unit()[6]}]'
+        string = "[" + " ".join(self.unit()) + "]"
         return string
--- tests/test_of_units.py
+++ tests/test_of_units.py
@@ -5,14 +5,14 @@ def test_creatible():
 
 def test_units():
     a = OF_units(SIunit='kgm-1s-2')
-    assert type(OF_units.values) is list
+    assert type(a.values) is list
 
-#def test_str():
-#    a = OF_units(SIunit='kg2')
-#    str(a)
+def test_str():
+    A = OF_units(SIunit='kg2')
+    assert type(A.SIunit) is str
 
 def test_str_is_what_we_want():
-    b = OF_units(SIunit='kg2cd1')
-    assert str(b) == '[ 2 0 0 0 0 0 1]'
+    b = OF_units(SIunit='kg2mol1')
+    assert str(b) == '[2 0 0 0 1 0 0]'
 
 #m a mol nejsou stejne
