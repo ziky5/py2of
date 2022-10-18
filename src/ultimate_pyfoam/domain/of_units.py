118b2918fa9ae83b582e9687b60ccf386e24fdaa
marstMUNI <483894@mail.muni.cz>
2022-10-18 10:40:24 +0000

    story so far

--- src/ultimate_pyfoam/domain/of_units.py
+++ src/ultimate_pyfoam/domain/of_units.py
@@ -4,9 +4,18 @@ import re
 @dataclass()
 class OF_units():
     SIunit: str #kgm-1s-2
+    values = []
+
+    def unit(self):
+        units = ['kg','m','s','K','mol','A','cd']
+        for u in units:
+            if u in self.SIunit:
+                z = re.search("("+f"[{u}]"+"{"+f"{len(u)}"+"})([0-9\\-]{1,5})", self.SIunit)
+                #print(z)
+                self.values.append(z[2])
+            else: self.values.append(0)
+        return self.values
 
     def __str__(self):
-        if 'm' in self.SIunit:
-            m = re.search('[m]{1}([0-9\-]{1,5})', self.SIunit) 
-        string = '[{kg}]'
+        string = f'[ {self.unit()[0]} {self.unit()[1]} {self.unit()[2]} {self.unit()[3]} {self.unit()[4]} {self.unit()[5]} {self.unit()[6]}]'
         return string
--- tests/test_of_units.py
+++ tests/test_of_units.py
@@ -1,8 +1,18 @@
 from ultimate_pyfoam.domain.of_units import OF_units
 
 def test_creatible():
-    OF_units(SIunit='Newton')
+    OF_units(SIunit='kgm-1s-2')
 
-def test_str():
-    a = OF_units(SIunit='Newton')
-    str(a)
+def test_units():
+    a = OF_units(SIunit='kgm-1s-2')
+    assert type(OF_units.values) is list
+
+#def test_str():
+#    a = OF_units(SIunit='kg2')
+#    str(a)
+
+def test_str_is_what_we_want():
+    b = OF_units(SIunit='kg2cd1')
+    assert str(b) == '[ 2 0 0 0 0 0 1]'
+
+#m a mol nejsou stejne
