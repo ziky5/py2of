03c51aa73c3e984c8b674085948209d6abdfb171
ARostekMU <rostek@mail.muni.cz>
2022-10-21 15:17:19 +0000

    ofdict inherits from userdict

--- src/ultimate_pyfoam/domain/of_dict.py
+++ src/ultimate_pyfoam/domain/of_dict.py
@@ -1,18 +1,18 @@
+from collections import UserDict
 from dataclasses import dataclass
-from typing import Mapping
 
 
-@dataclass()
-class OfDict():
-    name: str
-    content: Mapping
+@dataclass(init=False)
+class OfDict(UserDict):
+    pass
 
-    def __str__(self):
-        string = f"{self.name}"
-        string += "\n{"
 
-        for key, value in self.content.items():
-            string += f"\n    {key} {value};"
-
-        string += "\n}"
-        return string
+#    def __str__(self):
+#        string = f"{self.name}"
+#        string += "\n{"
+#
+#        for key, value in self.content.items():
+#            string += f"\n    {key} {value};"
+#
+#        string += "\n}"
+#        return string
--- tests/test_of_dict.py
+++ tests/test_of_dict.py
@@ -1,51 +1,50 @@
 from ultimate_pyfoam.domain.of_dict import OfDict
 
 
-
-def test_can_be_created():
-    OfDict(name="FoamFile", content={})
-
-
-def test_name_can_be_accessed():
-    a = OfDict(name="FoamFile", content={})
-    a.name
+def test_can_be_created() -> None:
+    OfDict()
 
 
-def test_str_can_be_used():
-    str(OfDict(name="FoamFile", content={}))
+def test_data_can_be_accessed() -> None:
+    a = OfDict({"FoamFile": {}})
+    a.data
 
 
-def test_str():
-    output = str(OfDict(name="FoamFile", content={"version":2.0}))
-    string = """\
-FoamFile
-{
-    version 2.0;
-}\
-"""
-    assert output == string
-
-
-def test_multiple_data_entries():
-    output = str(OfDict(name="FoamFile", content={"version":[2.0, 3.0]}))
-    string = """\
-FoamFile
-{
-    version 2.0 3.0;
-}\
-"""
-    assert output == string
-
-
-def test_nested_of_dict():
-    #TODO :how do we want to use nested dicts?
-    string = """\
-solvers
-{
-    p
-    {
-        solver PCG;
-    }
-}\
-"""
-    assert output == string
+# def test_str_can_be_used():
+#    str(OfDict(name="FoamFile", content={}))
+#
+#
+# def test_str():
+#    output = str(OfDict(name="FoamFile", content={"version": 2.0}))
+#    string = """\
+# FoamFile
+# {
+#    version 2.0;
+# }\
+# """
+#    assert output == string
+#
+#
+# def test_multiple_data_entries():
+#    output = str(OfDict(name="FoamFile", content={"version": [2.0, 3.0]}))
+#    string = """\
+# FoamFile
+# {
+#    version 2.0 3.0;
+# }\
+# """
+#    assert output == string
+#
+#
+# def test_nested_of_dict():
+#    # TODO :how do we want to use nested dicts?
+#    string = """\
+# solvers
+# {
+#    p
+#    {
+#        solver PCG;
+#    }
+# }\
+# """
+#    assert output == string
