a879f07ba7c4406c970aca4d7436affe5dfd667d
ARostekMU <rostek@mail.muni.cz>
2022-10-31 11:15:14 +0000

    ofDict is working, pre-commit passed

--- src/ultimate_pyfoam/domain/of_dict.py
+++ src/ultimate_pyfoam/domain/of_dict.py
@@ -1,11 +1,12 @@
 from collections import UserDict
 from dataclasses import dataclass
 from textwrap import indent
+from typing import Union
 
 
 @dataclass(init=False)
-class OfDict(UserDict[str, str]):
-    """muj string!"""
+class OfDict(UserDict[str, Union[str, float, int, bool, "OfDict"]]):
+    """OfDict."""
 
     def __str__(self) -> str:
         string = ""
