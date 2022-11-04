c3f403c844df00c9d702c2c28ece4d0f30c8bb42
ARostekMU <rostek@mail.muni.cz>
2022-11-04 12:03:56 +0000

    implemented __len__

--- src/ultimate_pyfoam/domain/of_list.py
+++ src/ultimate_pyfoam/domain/of_list.py
@@ -5,13 +5,15 @@ from typing import List
 @dataclass()
 class OfList:
     name: str
-    num_of_elements: int
     element_type: List[int]
     elements: List[int]
 
     def __str__(self) -> str:
         string = f"{self.name}"
-        string += "\tList[int]\n" + str(self.num_of_elements) + "\n(\n"
+        string += f"\tList[int]\n{len(self)}\n(\n"
         string += "\t[" + ",".join([str(i) for i in self.elements]) + "]"
         string += "\n);"
         return string
+
+    def __len__(self) -> int:
+        return len(self.elements)
--- tests/test_of_list.py
+++ tests/test_of_list.py
@@ -2,13 +2,13 @@ from ultimate_pyfoam.domain.of_list import OfList
 
 
 def test_create() -> None:
-    OfList("list", 5, [], [1, 2, 3])
+    OfList("list", [], [1, 2, 3])
 
 
 def test_print() -> None:
-    array = OfList("list", 5, [], [1, 2, 3])
+    array = OfList("list", [], [1, 2, 3])
     expected = """list\tList[int]
-5
+3
 (
 \t[1,2,3]
 );\
