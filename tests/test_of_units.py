17747315ca5bf1c6c09cb3557fb0db41c1320ff1
ARostekMU <rostek@mail.muni.cz>
2022-11-04 13:50:31 +0000

    preferred method

--- tests/test_of_units.py
+++ tests/test_of_units.py
@@ -5,6 +5,11 @@ def test_creatible() -> None:
     OfUnits()
 
 
-def test_str_is_what_we_want() -> None:
+def test_str() -> None:
+    b = OfUnits(kg=2, mol=1, s=5, cd=-3, A=-2, m=0, K=13)
+    assert str(b) == "[2 0 5 13 1 -2 -3]"
+
+
+def test_default_zeros() -> None:
     b = OfUnits(kg=2, mol=1)
     assert str(b) == "[2 0 0 0 1 0 0]"
