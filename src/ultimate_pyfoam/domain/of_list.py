4dd9c978b2f0f36704c1c7351d0d80e993fe21fc
ARostekMU <rostek@mail.muni.cz>
2022-11-04 12:52:37 +0000

    4 allowed element types

--- src/ultimate_pyfoam/domain/of_list.py
+++ src/ultimate_pyfoam/domain/of_list.py
@@ -1,16 +1,18 @@
 from dataclasses import dataclass
 from typing import List
 
+from ultimate_pyfoam.domain.of_tensor import OfTensor
+from ultimate_pyfoam.domain.of_vector import OfVector
+
 
 @dataclass()
 class OfList:
     name: str
-    element_type: List[int]
-    elements: List[int]
+    elements: List[int | float | OfVector | OfTensor]
 
     def __str__(self) -> str:
         string = f"{self.name}"
-        string += f"\tList[int]\n{len(self)}\n(\n"
+        string += f"\tList<{self.element_type}>\n{len(self)}\n(\n"
         for i in self.elements:
             string += f"{i}\n"
         string += ");"
@@ -18,3 +20,15 @@ class OfList:
 
     def __len__(self) -> int:
         return len(self.elements)
+
+    @property
+    def element_type(self) -> str:
+        python_element_type = type(self.elements[0])
+        if python_element_type == int or python_element_type == float:
+            return "scalar"
+        elif python_element_type == OfVector:
+            return "vector"
+        elif python_element_type == OfTensor:
+            return "tensor"
+        else:
+            raise TypeError()
--- tests/test_of_list.py
+++ tests/test_of_list.py
@@ -1,14 +1,16 @@
+import pytest
+
 from ultimate_pyfoam.domain.of_list import OfList
 
 
 def test_create() -> None:
-    OfList("listName", [], [1, 2, 3])
+    OfList("listName", [1, 2, 3])
 
 
-def test_print() -> None:
-    array = OfList("listName", [], [5, 12, 4])
+def test_print_int() -> None:
+    array = OfList("listName", [5, 12, 4])
     expected = """\
-listName\tList[int]
+listName\tList<scalar>
 3
 (
 5
@@ -18,3 +20,9 @@ listName\tList[int]
 """
     print(str(array))
     assert str(array) == expected
+
+
+def test_print_str() -> None:
+    with pytest.raises(TypeError):
+        array = OfList("listName", ["aaa"])  # type: ignore
+        str(array)
