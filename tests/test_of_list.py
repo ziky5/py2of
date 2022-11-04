38ac11d2969d23d526c49a291fc23e2fd636d25e
ARostekMU <rostek@mail.muni.cz>
2022-11-04 12:58:33 +0000

    of_vectors as list elements

--- tests/test_of_list.py
+++ tests/test_of_list.py
@@ -1,6 +1,7 @@
 import pytest
 
 from ultimate_pyfoam.domain.of_list import OfList
+from ultimate_pyfoam.domain.of_vector import OfVector
 
 
 def test_create() -> None:
@@ -16,6 +17,22 @@ listName\tList<scalar>
 5
 12
 4
+);\
+"""
+    print(str(array))
+    assert str(array) == expected
+
+
+def test_print_vector() -> None:
+    array = OfList(
+        "listName", [OfVector(x=1.1, y=5.0, z=0.0), OfVector(x=1.0, y=4.0, z=2.5)]
+    )
+    expected = """\
+listName\tList<vector>
+2
+(
+(1.1 5.0 0.0)
+(1.0 4.0 2.5)
 );\
 """
     print(str(array))
