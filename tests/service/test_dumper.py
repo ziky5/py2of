f834581112e554a88e14ce76073645f9021a8835
ARostekMU <rostek@mail.muni.cz>
2022-11-14 16:48:30 +0000

    path and overwrite parameters should be in write method

--- tests/service/test_dumper.py
+++ tests/service/test_dumper.py
@@ -7,12 +7,12 @@ from ultimate_pyfoam.service.dumper import Dumper
 
 
 def test_init(tmp_path: Path) -> None:
-    Dumper(path=tmp_path / "dumpedFile", content=OfDict())
+    Dumper(content=OfDict())
 
 
 def test_can_write_file(tmp_path: Path) -> None:
-    d = Dumper(path=tmp_path / "dumpedFile", content=OfDict())
-    d.write()
+    d = Dumper(content=OfDict())
+    d.write(path=tmp_path / "dumpedFile")
     assert (tmp_path / "dumpedFile").exists()
 
 
@@ -20,45 +20,43 @@ def test_writes_content_correctly(tmp_path: Path) -> None:
     string = "application scalarTransportFoam;"
     dct = OfDict({"application": "scalarTransportFoam"})
 
-    d = Dumper(path=tmp_path / "controlDict", content=dct)
-    d.write()
+    d = Dumper(content=dct)
+    d.write(path=tmp_path / "controlDict")
 
     written_string = Path(tmp_path / "controlDict").read_text().strip()
     assert string == written_string
 
 
 def test_refuses_to_overwrite_by_default(tmp_path: Path) -> None:
-    d = Dumper(path=tmp_path / "dumpedFile", content=OfDict())
-    d.write()
+    d = Dumper(content=OfDict())
+    d.write(path=tmp_path / "dumpedFile")
 
     with pytest.raises(FileExistsError):
-        d.write()
+        d.write(path=tmp_path / "dumpedFile")
 
 
 def test_overwrite_when_specified(tmp_path: Path) -> None:
-    d = Dumper(path=tmp_path / "dumpedFile", content=OfDict(), overwrite=True)
-    d.write()
-    d.write()
+    d = Dumper(content=OfDict())
+    d.write(path=tmp_path / "dumpedFile")
+    d.write(path=tmp_path / "dumpedFile", overwrite=True)
 
 
 def test_create_directory_in_path_if_nonexistent(tmp_path: Path) -> None:
-    d = Dumper(path=tmp_path / "constant/dumpedFile", content=OfDict())
-    d.write()
+    d = Dumper(content=OfDict())
+    d.write(path=tmp_path / "constant/dumpedFile")
 
     assert (tmp_path / "constant" / "dumpedFile").exists()
 
 
 def test_create_multiple_directories_in_path_if_nonexistent(tmp_path: Path) -> None:
-    d = Dumper(
-        path=tmp_path / "constant/one_more_directory/dumpedFile", content=OfDict()
-    )
-    d.write()
+    d = Dumper(content=OfDict())
+    d.write(path=tmp_path / "constant/one_more_directory/dumpedFile")
 
     assert (tmp_path / "constant" / "one_more_directory" / "dumpedFile").exists()
 
 
 def test_cannot_write_to_directory(tmp_path: Path) -> None:
-    d = Dumper(path=tmp_path, content=OfDict())
+    d = Dumper(content=OfDict())
 
     with pytest.raises(IsADirectoryError):
-        d.write()
+        d.write(path=tmp_path)
