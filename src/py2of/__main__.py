837dfd560f06d35e8c88642fc7384fbc6a9c2a30
ziky5 <zikan.p@gmail.com>
2022-12-04 17:46:32 +0000

    context manager for importing module

--- src/py2of/__main__.py
+++ src/py2of/__main__.py
@@ -2,10 +2,23 @@
 import sys
 from importlib import import_module
 from pathlib import Path
+from typing import Any
 
 import click
 
 from py2of.service.case_dumper import CaseDumper
+
+
+class ImportModule:
+    def __init__(self, path: Path) -> None:
+        self.path = path
+
+    def __enter__(self) -> Any:
+        sys.path.insert(0, str(self.path.parent))
+        return import_module(self.path.stem)
+
+    def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
+        del sys.path[0]
 
 
 @click.group()
@@ -19,10 +32,8 @@ def main() -> None:
 @click.option("--dump-dir", default="of_case", type=click.Path(path_type=Path))
 @click.option("--case-mod-attr", default="case")
 def dump(content_file: Path, dump_dir: Path, case_mod_attr: str) -> None:
-    sys.path.insert(0, str(content_file.parent))
-    module = import_module(content_file.stem)
-    CaseDumper(getattr(module, case_mod_attr)).dump(dump_dir)
-    del sys.path[0]
+    with ImportModule(content_file) as module:
+        CaseDumper(getattr(module, case_mod_attr)).dump(dump_dir)
 
 
 if __name__ == "__main__":
